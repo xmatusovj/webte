16c126f8a1cb6d04bd0464a0236331cc93b40cdf
ThomDhan <44169375+ThomDhan@users.noreply.github.com>
2019-05-13 14:05:05 +0200

    Add files via upload

---
+++ uloha2help2.php
@@ -0,0 +1,38 @@
+<?php
+
+	$idStudenta =  $_POST["idStudenta"];
+	$suhlasAky =  $_POST["suhlasAky"];
+	$predmetPrihlaseneho = $_POST["predmetPrihlaseneho"];
+
+/*
+echo "<hr>idStudenta: ";
+echo $idStudenta;
+echo "<hr>suhlasAky: ";
+echo $suhlasAky;
+echo "<hr>predmetPrihlaseneho: ";
+echo $predmetPrihlaseneho;
+echo "<hr>";
+*/
+
+
+	require_once('config.php');
+
+	$conn = new mysqli($servername, $username, $password, $dbu2);
+	// Check connection
+	if ($conn->connect_error) {
+		//die("Connection failed: " . $conn->connect_error);
+	} 
+
+	$sql123 = " UPDATE student SET suhlas = '".$suhlasAky."' WHERE id = '".$idStudenta."'  AND predmet = '".$predmetPrihlaseneho."' ";
+
+	
+
+	//if ($conn->query($sql123) === TRUE) { echo "ano";}	else{ echo "nie";}
+	if ($conn->query($sql123) === TRUE) { echo "";}	else{ echo "";}
+
+
+	$conn->close();
+
+
+header("Location:myuloha2.php");
+?>
